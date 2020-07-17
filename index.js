1d642047cdd785affa535835bac4112ffde68e5c
LanGvest <61324477+LanGvest@users.noreply.github.com>
2020-07-17 21:22:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ bot.command(/^какой сейчас урок(\.|\?)?$/i, (ctx) => {
  
  let date = ctx.message.date*1000;
     date1 = new Date(date);
-    ctx.reply(date1.toString());
+    ctx.reply("N1: " + date1.toString());
 
 });
  
