6f0601933b66f93d6d0c2146e505d6f7e7af4fd3
LanGvest <61324477+LanGvest@users.noreply.github.com>
2020-07-17 21:31:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const bot = new VkBot(process.env.TOKEN);
  
 bot.command(/^какой сейчас урок(\.|\?)?$/i, (ctx) => {
  
- let date = ctx.message.date*1000;
-    date1 = new Date(date);
-    ctx.reply("N1: " + date1.toString() + " --- " + date);
+ let date = new Date();
+    ctx.reply("N1: " + date.getTimezoneOffset());
 
 });
  
