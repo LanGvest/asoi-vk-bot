bbeffd6983ec455818679d21ecbd8896d93bd663
LanGvest <61324477+LanGvest@users.noreply.github.com>
2020-07-17 20:15:08 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const VkBot = require('node-vk-bot-api');
+ 
+const bot = new VkBot(process.env.TOKEN);
+ 
+bot.command(/^какой сейчас урок(\.|\?)?$/i, (ctx) => {
+    
+    let date = new Date();
+    console.log(date.toString())
+    ctx.reply("time");
+});
+ 
+bot.startPolling();
