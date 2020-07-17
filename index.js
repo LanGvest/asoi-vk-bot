3c8250a7d7a333fe52f92bddcfe5b4132c30a5ed
LanGvest <61324477+LanGvest@users.noreply.github.com>
2020-07-17 22:13:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const VkBot = require('node-vk-bot-api');
 const bot = new VkBot(process.env.TOKEN);
  
 bot.command(/^какой сейчас урок(\.|\?)?$/i, (ctx) => {
- let d = new Date();
- let date = new Date(d.getTime()+28800000);
- ctx.reply("T12: " + date.getTimezoneOffset() + "::" + date.getTime()+"::"+date.toString());
-
+ let date = new Date(new Date().getTime() + 10800000);
+ ctx.reply(date.toString());
 });
  
 bot.startPolling();
